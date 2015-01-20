b57272e348041e82e391a6897467ecb5c8786066
azu <azuciao@gmail.com>
2015-01-20 18:34:56 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# todo-app-jquery-to-backbone
+
+Refactoring todo app - migrate jquery to backbone.
+
+- [slides/refactoring at gh-pages · hokaccha/slides](https://github.com/hokaccha/slides/tree/gh-pages/refactoring "slides/refactoring at gh-pages · hokaccha/slides")
+
+Use [testium](https://github.com/groupon-testium/testium "testium") for E2E testing.
+
+## Installation
+
+```
+npm install
+```
+
+## Usage
+
+```
+npm test
+```
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+MIT
---
+++ test/app-e2e-test.js
@@ -0,0 +1,58 @@
+// LICENSE : MIT
+"use strict";
+var injectBrowser = require('testium/mocha');
+var assert = require("power-assert");
+var browser;
+function addTodo(text) {
+    browser.setValue('.todoText', text);
+    browser.click('.todoBtn');
+}
+describe("app-test", function () {
+    var text = 'todo test';
+    before(injectBrowser());
+    beforeEach(function () {
+        browser = this.browser;
+        this.browser.navigateTo("/");
+    });
+    context("when テキストボックスに文字を入れて送信した時", function () {
+        beforeEach(function () {
+            addTodo(text)
+        });
+        it("should li要素が作成されている", function () {
+            var list = browser.getElements('.todoList li');
+            assert(list.length > 0);
+        });
+
+        it("should リストアイテムのテキストは送信したものと一致している", function () {
+            browser.assert.elementHasText('.todoList li', text)
+        });
+    });
+    describe("todoについて", function () {
+        beforeEach(function () {
+            addTodo(text);
+        });
+        context("checkboxをクリックしたら", function () {
+            it("should `is-complete`が追加される", function () {
+                browser.click('.todoList li input[type="checkbox"]');
+                browser.assert.elementExists(".is-complete");
+            });
+        });
+        context("removeBtnをクリックして、confirmでキャンセルしても", function () {
+            it("li要素は消えない", function () {
+                // confirmがfalseを返すようにする = キャンセル
+                browser.evaluate("return window.confirm = function() { return " + false + "; };");
+
+                browser.click('.todoList li .removeBtn');
+                browser.assert.elementExists(".todoList li");
+            });
+        });
+        context("removeBtnをクリックしてconfirmでOKしたら", function () {
+            it("li要素が消える", function () {
+                // confirmがfalseを返すようにする = キャンセル
+                browser.evaluate("return window.confirm = function() { return " + true + "; };");
+                browser.click('.todoList li .removeBtn');
+                browser.assert.elementDoesntExist(".todoList li");
+            });
+        });
+    });
+});

1b229f744769bb24b7c41b699654f42089dc8fd4
azu <azuciao@gmail.com>
2015-01-21 11:27:18 +0900

    feat(app): add collection of TodoItem

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 import TodoItemView from "./todo-item-view.js"
 import TodoItem from "./todo-item-model.js"
+import TodoItemList from "./todo-item-collection.js"
 
 $(function () {
     var $form = $('.todoForm');
     var $list = $('.todoList');
+    var todoItemList = new TodoItemList();
+    todoItemList.on("add", function (todoItem) {
+        var item = new TodoItemView({model: todoItem});
+        var list = item.render().el;
+        $list.append(list);
+    });
 
     function createTodoItem(text) {
-        var model = new TodoItem({
+        todoItemList.add({
             title: text
         });
-        var item = new TodoItemView({model});
-        return item.render().el;
     }
 
     $form.on('submit', function (e) {
@@ -18,9 +23,7 @@ $(function () {
 
         var $input = $('input[type="text"]');
         var val = $input.val();
-        var $li = createTodoItem(val);
-
-        $list.append($li);
+        createTodoItem(val);
 
         $input.val('');
     });
